61297d18ea9e57ff5d84d7746378fc495e5dc3e1
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2021-02-27 12:21:31 +0530

    Update contacts.dart

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -1,1 +1,9 @@
-//Creating simple class file for demo Appclass Contact{  final String name, mobile, time;  Contact(this.name, this.mobile, this.time);}
+//Creating simple class file for demo App
+
+class Contact{
+
+  final String name, mobile, time;
+
+  Contact(this.name, this.mobile, this.time);
+
+}
